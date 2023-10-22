48a7a650084a42187f96e2aa69c8c27d1615c020
kemosaaabe <sanchellos27@mail.ru>
2023-10-22 13:45:27 +0300

    added custom field

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ import {
   EditGuesser,
   ShowGuesser,
 } from "react-admin";
+
+import { UserList } from "./users";
+
 import { dataProvider } from "./dataProvider";
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+    <Resource name="users" list={UserList} />
   </Admin>
 );
---
+++ src/users.tsx
@@ -0,0 +1,36 @@
+import { useMediaQuery, Theme } from "@mui/material";
+import {
+  Datagrid,
+  SimpleList,
+  EmailField,
+  List,
+  TextField,
+  UrlField,
+} from "react-admin";
+
+import MyUrlField from "./MyUrlField";
+
+export const UserList = () => {
+  const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
+
+  return (
+    <List>
+      {isSmall ? (
+        <SimpleList
+          primaryText={(record) => record.name}
+          secondaryText={(record) => record.username}
+          tertiaryText={(record) => record.email}
+        ></SimpleList>
+      ) : (
+        <Datagrid rowClick="edit">
+          <TextField source="id" />
+          <TextField source="name" />
+          <EmailField source="email" />
+          <TextField source="phone" />
+          <MyUrlField source="website" />
+          <TextField source="company.name" />
+        </Datagrid>
+      )}
+    </List>
+  );
+};
